4d9a883a72610cdc01f34f48c672ed1c572752af
Maulidan Yulianto <maulidan.ksl@gmail.com>
2017-09-10 21:41:34 +0700

    udpdate

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')().listen(server);
+var cors = require('cors');
 
+app.use(cors());
 io.on("connection", function(socket){
     console.log("A new client has connected with the id " + socket.id + "!");
     
